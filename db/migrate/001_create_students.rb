a2d5e1959e64385e9961182873da223718fc878e
Megan Comeau <mlcomeau99@gmail.com>
2020-06-09 19:24:49 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,9 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
-  def change 
+  def change
     create_table :students do |t|
       t.string :first_name
-      t.string :last_name 
-    end 
-  end 
-  
+      t.string :last_name
+    end
+  end
+
 end 
